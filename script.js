9196dd96fef235f70b0541ab0d33816ece6c5da5
alifsyfulislam <alifsyfulislam@gmail.com>
2019-05-04 15:15:38 +0600

    added

---
+++ script.js
@@ -0,0 +1,271 @@
+var sehriTime = [
+    "3:52","3:51","3:50","3:50","3:49","3:49","3:48","3:48","3:47","3:47",
+    "3:46","3:46","3:45","3:45","3:44","3.44","3:43","3:43","3:42","3:42",
+    "3:41","3:41","3:40","3:40","3:40","3:40","3:39","3:39","3:39","3:39"
+];
+
+var iftarTime = [
+    "18:34","18:34","18:35","18:35","18:36","18:36","18:36","18:37","18:37","18:38",
+    "18:38","18:38","18:39","18:39","18:40","18:40","18:41","18:42","18:42","18:42",
+    "18:43","18:43","18:44","18:44","18:45","18:45","18:46","18:46","18:47","18:47"
+];
+
+var fajarTime = [
+    "3:58","3:57","3:56","3:56","3:55","3:55","3:55","3:54","3:54","3:53",
+    "3:53","3:52","3:52","3:51","3:50","3:50","3:50","3:49","3:49","3:48",
+    "3:48","3:47","3:47","3:46","3:46","3:46","3:45","3:45","3:45","3:45"
+];
+
+var johurTime = [
+    "11:58","11:58","11:58","11:58","11:58","11:58","11:58","11:58","11:58","11:58",
+    "11:58","11:58","11:59","11:59","11:59","11:59","11:59","11:59","11:59","11:59",
+    "11:59","11:59","11:59","11:59","12:0","12:0","12:1","12:1","12:1","12:1"
+];
+
+var asarTime = [
+    "16:31","16:31","16:31","16:31","16:31","16:32","16:32","16:32","16:32","16:32",
+    "16:32","16:32","16:33","16:33","16:33","16:33","16:33","16:33","16:33","16:34",
+    "16:34","16:34","16:34","16:34","16:34","16:35","16:35","16:35","16:35","16:35"
+];
+
+var magribTime = [
+    "18:34","18:34","18:35","18:35","18:36","18:36","18:36","18:37","18:37","18:38",
+    "18:38","18:38","18:39","18:39","18:40","18:40","18:41","18:42","18:42","18:42",
+    "18:43","18:43","18:44","18:44","18:45","18:45","18:46","18:46","18:47","18:47"
+];
+
+
+var eshaTime = [
+    "19:58","19:58","19:58","19:58","19:58","19:58","20:1","20:1","20:1","20:1",
+    "20:1","20:1","20:1","20:1","20:1","20:1","20:1","20:6","20:6","20:6",
+    "20:6","20:9","20:9","20:9","20:11","20:11","20:11","20:11","20:11","20:13"
+];
+
+var hadithText = [
+    "যখন রমযান মাসের আগমন ঘটে, তখন জান্নাতের দরজাসমূহ খুলে দেওয়া হয় এবং জাহান্নামের দরজাসমূহ বন্ধ করে দেওয়া হয়। আর শয়তানদেরকে শৃঙ্খলাবদ্ধ করা হয়।",
+    "যে ব্যক্তি অসুস্থতা ও সফর ব্যতীত ইচ্ছাকৃতভাবে রমযানের একটি রোযাও ভঙ্গ করে, সে আজীবন রোযা রাখলেও ঐ রোযার হক আদায় হবে না",
+    "তোমাদের কেউ যখন রোযা রাখে তখন সে যেন অশালীন কথাবার্তা না বলে ও হৈ চৈ না করে।",
+    "যে ঈমান ও বিশ্বাস সহকারে সওয়াবের আশায় শবে কদরে নামায পড়ে এবং রমজানের রোযা রাখবে,তার অতীতের সকল গুনাহ মাফ করে দেওয়া হবে।",
+    "এমন দু’টি মাস আছে যার উভয়টি (পর পর) ঘাটতি অর্থাৎ ঊনত্রিশ দিন হয় না।আর তা হল ঈদের দু’টি মাস রমজান এবং যিলহজ্ব।",
+    "রমজানের একদিন বা দু’দিন পূর্বে নফল রোযা রাখা যাবে না। তবে কেউ যদি প্রতিমাসে এ রোযা রাখতে অভ্যস্ত ,তাহলে রাখতে পার।",
+    "তোমরা সেহরী খাও। কেননা সেহরী খাওয়ায় বরকত লাভ হয়। ",
+    "রোযাদার যদি ভুল করে কিছু খায় বা পান করে, তা হলে সে (ইফতার না করে) রোযা পূর্ণ করবে।",
+    "মৃত ব্যক্তির উপর রোযার কাজা থাকলে অভিভাবক তার পক্ষ থেকে তা আদায় করবে।",
+    "যতদিন লোকেরা তাড়াতাড়ি (সূর্যাস্তের সাথে সাথে) ইফতার করবে , ততদিন কল্যাণ থেকে বঞ্চিত হবে না।",
+    "যখন রমযান মাসের আগমন ঘটে, তখন জান্নাতের দরজাসমূহ খুলে দেওয়া হয় এবং জাহান্নামের দরজাসমূহ বন্ধ করে দেওয়া হয়। আর শয়তানদেরকে শৃঙ্খলাবদ্ধ করা হয়।",
+    "যে ব্যক্তি অসুস্থতা ও সফর ব্যতীত ইচ্ছাকৃতভাবে রমযানের একটি রোযাও ভঙ্গ করে, সে আজীবন রোযা রাখলেও ঐ রোযার হক আদায় হবে না",
+    "তোমাদের কেউ যখন রোযা রাখে তখন সে যেন অশালীন কথাবার্তা না বলে ও হৈ চৈ না করে।",
+    "যে ঈমান ও বিশ্বাস সহকারে সওয়াবের আশায় শবে কদরে নামায পড়ে এবং রমজানের রোযা রাখবে,তার অতীতের সকল গুনাহ মাফ করে দেওয়া হবে।",
+    "এমন দু’টি মাস আছে যার উভয়টি (পর পর) ঘাটতি অর্থাৎ ঊনত্রিশ দিন হয় না।আর তা হল ঈদের দু’টি মাস রমজান এবং যিলহজ্ব।",
+    "রমজানের একদিন বা দু’দিন পূর্বে নফল রোযা রাখা যাবে না। তবে কেউ যদি প্রতিমাসে এ রোযা রাখতে অভ্যস্ত ,তাহলে রাখতে পার।",
+    "তোমরা সেহরী খাও। কেননা সেহরী খাওয়ায় বরকত লাভ হয়। ",
+    "রোযাদার যদি ভুল করে কিছু খায় বা পান করে, তা হলে সে (ইফতার না করে) রোযা পূর্ণ করবে।",
+    "মৃত ব্যক্তির উপর রোযার কাজা থাকলে অভিভাবক তার পক্ষ থেকে তা আদায় করবে।",
+    "যতদিন লোকেরা তাড়াতাড়ি (সূর্যাস্তের সাথে সাথে) ইফতার করবে , ততদিন কল্যাণ থেকে বঞ্চিত হবে না।",
+    "যখন রমযান মাসের আগমন ঘটে, তখন জান্নাতের দরজাসমূহ খুলে দেওয়া হয় এবং জাহান্নামের দরজাসমূহ বন্ধ করে দেওয়া হয়। আর শয়তানদেরকে শৃঙ্খলাবদ্ধ করা হয়।",
+    "যে ব্যক্তি অসুস্থতা ও সফর ব্যতীত ইচ্ছাকৃতভাবে রমযানের একটি রোযাও ভঙ্গ করে, সে আজীবন রোযা রাখলেও ঐ রোযার হক আদায় হবে না",
+    "তোমাদের কেউ যখন রোযা রাখে তখন সে যেন অশালীন কথাবার্তা না বলে ও হৈ চৈ না করে।",
+    "যে ঈমান ও বিশ্বাস সহকারে সওয়াবের আশায় শবে কদরে নামায পড়ে এবং রমজানের রোযা রাখবে,তার অতীতের সকল গুনাহ মাফ করে দেওয়া হবে।",
+    "এমন দু’টি মাস আছে যার উভয়টি (পর পর) ঘাটতি অর্থাৎ ঊনত্রিশ দিন হয় না।আর তা হল ঈদের দু’টি মাস রমজান এবং যিলহজ্ব।",
+    "রমজানের একদিন বা দু’দিন পূর্বে নফল রোযা রাখা যাবে না। তবে কেউ যদি প্রতিমাসে এ রোযা রাখতে অভ্যস্ত ,তাহলে রাখতে পার।",
+    "তোমরা সেহরী খাও। কেননা সেহরী খাওয়ায় বরকত লাভ হয়। ",
+    "রোযাদার যদি ভুল করে কিছু খায় বা পান করে, তা হলে সে (ইফতার না করে) রোযা পূর্ণ করবে।",
+    "মৃত ব্যক্তির উপর রোযার কাজা থাকলে অভিভাবক তার পক্ষ থেকে তা আদায় করবে।",
+    "যতদিন লোকেরা তাড়াতাড়ি (সূর্যাস্তের সাথে সাথে) ইফতার করবে , ততদিন কল্যাণ থেকে বঞ্চিত হবে না।"
+];
+
+
+
+
+
+var audioFile = document.getElementById('audioFile');
+var audioFile2 = document.getElementById('audioFile2');
+
+
+
+
+setInterval( function () {
+    var today = new Date();
+    if (today.getHours()<19 && today.getHours()>13){
+        checkIftarTime();
+    }
+    else if (today.getHours()<=4 && today.getHours()>=0) {
+        checkSehriTime();
+    }
+    else {
+        document.getElementById('subtitle').innerHTML = "Clock";
+        document.getElementById('counter').innerHTML = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+    }
+    checkSalahTime();
+    checkSalahBoard();
+    checkHadith();
+    checkPosition();
+},1000);
+
+
+function checkIftarTime() {
+    var today = new Date();
+    var currDate = today.getDate();
+    if (currDate){
+        var difference = 0;
+        var number = iftarTime[currDate].split(":");
+        var givenTime = (parseInt(number[0]*60)+parseInt(number[1]))*60;
+        var lastTIme = (((today.getHours()*60+today.getMinutes())*60)+today.getSeconds());
+        difference = givenTime - lastTIme;
+        if (difference<4*60*60 && difference>=0){
+            document.getElementById('subtitle').innerHTML = "Iftar Countdown";
+            document.getElementById('counter').innerHTML = printTimer(difference);
+        }
+    }
+}
+
+function checkSehriTime() {
+    var today = new Date();
+    var currDate = today.getDate();
+    if (currDate){
+        var difference = 0;
+        var number = sehriTime[currDate].split(":");
+        var givenTime = (parseInt(number[0]*60)+parseInt(number[1]))*60;
+        var lastTIme = (((today.getHours()*60+today.getMinutes())*60)+today.getSeconds());
+        difference = givenTime - lastTIme;
+        if (difference<4*60*60 && difference>=0){
+            document.getElementById('subtitle').innerHTML = "Sehri Countdown";
+            document.getElementById('counter').innerHTML = printTimer(difference);
+        }
+    }
+}
+
+
+function printTimer(sec) {
+    hr = Math.floor(sec / 3600) % 24;
+    mm = Math.floor(sec / 60) % 60;
+    ss = sec % 60;
+
+    var x = hr < 10? "0"+hr : hr;
+    var y = mm < 10? "0"+mm : mm;
+    var z = ss < 10? "0"+ss : ss;
+    return ( x+":"+y+":"+z)
+}
+
+
+
+function checkSalahTime() {
+    var today = new Date();
+    var currHour = today.getHours();
+    var currMin = today.getMinutes();
+    var currSec = today.getSeconds();
+    var currDate = today.getDate();
+    if (currDate){
+        var fajarSalah = fajarTime[currDate]+":"+"0";
+        var johurSalah = johurTime[currDate]+":"+"0";
+        var asarSalah = asarTime[currDate]+":"+"0";
+        var mafribSalah = magribTime[currDate]+":"+"0";
+        var eshaSalah = eshaTime[currDate]+":"+"0";
+        var currentClock = currHour+":"+currMin+":"+currSec;
+        if (fajarSalah == currentClock){
+            audioFile.autoplay = true;
+            audioFile.load();
+            console.log("match");
+        }
+        else if (johurSalah == currentClock) {
+            audioFile2.autoplay = true;
+            audioFile2.load();
+            console.log("johur");
+        }
+
+        else if (asarSalah == currentClock) {
+            audioFile2.autoplay = true;
+            audioFile2.load();
+            console.log("asar");
+        }
+
+        else if (mafribSalah == currentClock){
+            audioFile2.autoplay = true;
+            audioFile2.load();
+            console.log("magrib");
+        }
+
+        else if (eshaSalah == currentClock){
+            audioFile2.autoplay = true;
+            audioFile2.load();
+            console.log("esha");
+        }
+    }
+}
+
+
+
+function checkSalahBoard() {
+    var today = new Date();
+    var currDate = today.getDate();
+    setTimeout(function () {
+        document.getElementById('fajr').innerText = "ফজর\n"+fajarTime[currDate];
+        document.getElementById('johur').innerText = "জহুর\n"+johurTime[currDate];
+        document.getElementById('asar').innerText = "আসর\n"+asarTime[currDate];
+        document.getElementById('magrib').innerText = "মাগরিব\n"+magribTime[currDate];
+        document.getElementById('esha').innerText = "এশা\n"+eshaTime[currDate];
+    },2e3)
+}
+
+
+function checkHadith() {
+    var today = new Date();
+    var currDate = today.getDate();
+    if (currDate){
+        document.getElementById('hadis').innerHTML = hadithText[currDate];
+    }
+}
+
+
+function checkPosition() {
+    getLocation();
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+function showPosition(position) {
+    var low = (position.coords.latitude);
+    var high = (position.coords.longitude);
+    console.log(low +":"+high);
+    // if (low == "23.777" && high == "90.415"){
+    //     console.log("dhk");
+    // }
+    // else if (low == "22.338" && high == "91.831") {
+    //     console.log("ctg");
+    // }
+    // else if (low == "22.809" && high == "89.564") {
+    //     console.log("khul");
+    // }
+    // else if (low == "24.374" && high == "88.601") {
+    //     console.log("raj");
+    // }
+    // else if (low == "24.899" && high == "91.871") {
+    //     console.log("sly");
+    // }
+    // else if (low == "22.704" && high == "90.370") {
+    //     console.log("bar");
+    // }
+    // else if (low == "25.746" && high == "89.251") {
+    //     console.log("ranng");
+    // }
+    // else if (low == "24.756" && high == "90.406") {
+    //     console.log("mymen");
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
